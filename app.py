181e607297df80eff83fc26ea6d8c23d7c7ed85e
Mayke Alex Miranda Plaça <mayke.suporte+github@gmail.com>
2022-11-07 13:31:36 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ ufs = ['ac', 'al','ap','am','ba','ce','df','es','zz','go','ma','mt','ms','mg','p
 turnosUrls = ['https://resultados.tse.jus.br/oficial/app/index.html#/eleicao;e=e544;uf=ESTADO/dados-de-urna/boletim-de-urna',
               'https://resultados.tse.jus.br/oficial/app/index.html#/eleicao;e=e545;uf=ESTADO/dados-de-urna/boletim-de-urna']
 
+turnos=['e544', 'e544']
+
 for t, turnoUrl in enumerate(turnosUrls):
     if (t == 0): #pular para o segunto turno
         continue
@@ -14,5 +16,6 @@ for t, turnoUrl in enumerate(turnosUrls):
         url = turnoUrl.replace('=ESTADO/', '={}/'.format(uf))
         tse.acessTse(url)
         tse.uf_atual = '{}'.format(uf)
+        tse.turno_atual = '{}'.format(turnos[t])
         tse.selecionaLocal()
         print('botao')
