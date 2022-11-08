5cfca6250c59ecf7c2cc856f26e32380a46cffd1
2maykealex <mayke.suporte@gmail.com>
2022-11-08 17:48:45 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,31 @@
 from tseFunctions import TseFunctions
-tse = TseFunctions()
+from decouple import config
 
+tse = TseFunctions()
 turnos=['e544', 'e544']
 ufs = ['ac', 'al','ap','am','ba','ce','df','es','zz','go','ma','mt','ms','mg','pr','pb','pa','pe','pi','rj','rn','rs','ro','rr','sc','se','sp','to',]
 turnosUrls = ['https://resultados.tse.jus.br/oficial/app/index.html#/eleicao;e=e544;uf=ESTADO/dados-de-urna/boletim-de-urna',
               'https://resultados.tse.jus.br/oficial/app/index.html#/eleicao;e=e545;uf=ESTADO/dados-de-urna/boletim-de-urna']
 
+estadosOk = (config('ESTADOS')).split(',')
+municipio = int(config('MUNICIPIO'))
+turno = config('TURNO')
+try:
+    turno = int(turno)
+except:
+    turno = 0
+
+tse = TseFunctions()
+tse.countCity = municipio
+
 for t, turnoUrl in enumerate(turnosUrls):
-    if (t == 0): #pular para o segunto turno
+    if (t != int(turno)):
         continue
 
     for uf in ufs:
+        if uf in estadosOk:
+            continue
+
         print('\n---- {} TURNO ----'.format(t+1))
         url = turnoUrl.replace('=ESTADO/', '={}/'.format(uf))
         tse.acessTse(url)
